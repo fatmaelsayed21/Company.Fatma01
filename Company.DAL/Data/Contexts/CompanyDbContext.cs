2a16d54b27455c683b65fad0d9b2fc3ef37e33d3
fatma <fatma@LAPTOP-65F0P1MG>
2025-04-02 12:15:57 +0200

    Add project files.

---
+++ Company.DAL/Data/Contexts/CompanyDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Company.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company.DAL.Data.Contexts
+{
+    public class CompanyDbContext : DbContext
+    {
+        public CompanyDbContext() : base()
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder = modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server= LAPTOP-65F0P1MG; Database=Company21; Trusted_Connection= True; TrustServerCertificate=True");
+        }
+
+        public DbSet<Department> Departments { get; set; }
+
+    }   }
