a2a614bf2207e2624f83a9fefb1bea66f69db26e
fatma <fatma@LAPTOP-65F0P1MG>
2025-04-02 22:19:51 +0200

    Create for Create Action and view

--- Company.Fatma01/Controllers/DepartmentController.cs
+++ Company.Fatma01/Controllers/DepartmentController.cs
@@ -1,5 +1,7 @@
 ﻿using Company.BLL.Interfaces;
 using Company.BLL.Repositories;
+using Company.DAL.Models;
+using Company.PL.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.PL.Controllers
@@ -18,6 +20,34 @@ namespace Company.PL.Controllers
           
             var departments = _departmentRepository.GetAll();
             return View(departments);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+            if (ModelState.IsValid)    //server side validation
+            {
+                var department= new Department()
+                {
+                    Code = model.Code,
+                    Name = model.Name,
+                    CreateAt = model.CreateAt
+
+                };
+
+               var count= _departmentRepository.Add(department);
+                if (count > 0)
+                { 
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(model);
         }
     }
 }
---
+++ Company.Fatma01/Dtos/CreateDepartmentDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.Build.Framework;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace Company.PL.Dtos
+{
+    public class CreateDepartmentDto
+    {
+        [Required(ErrorMessage="Code is required !")]
+        public string Code { get; set; }
+        [Required(ErrorMessage = "Name is required !")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Create At is required !")]
+        public DateTime CreateAt { get; set; }
+    }
+}
